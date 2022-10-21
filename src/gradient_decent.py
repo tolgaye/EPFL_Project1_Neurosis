1ca0ddf5548ec54b73dd33ecab6cd884b4b70e0a
BatuhanBertan <batuhan.karagoz@epfl.ch>
2022-10-21 11:39:04 +0200

    GD_SGD_LS_RR

---
+++ src/gradient_decent.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Problem Sheet 2.
+Gradient Descent
+"""
+from cost import calculate_mse
+
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
