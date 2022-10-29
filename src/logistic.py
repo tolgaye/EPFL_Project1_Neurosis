1a7f972d9eb23e49d59835725301fe612ba9b40c
tolgaye <95972742+tolgaye@users.noreply.github.com>
2022-10-29 17:32:09 +0200

    Update logistic.py

--- src/logistic.py
+++ src/logistic.py
@@ -2,8 +2,8 @@ import numpy as np
 import math
 from typing import  Tuple
 from random import randrange
-from src.loss import *
-from src.gradient import *
+from loss import *
+from gradient import *
 
 
 def sigmoid(t):
